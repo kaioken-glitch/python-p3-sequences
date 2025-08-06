1673a02885611ecc220c0ca5dd951dcff4f4dc78
Cyprian <lightningcyranus@gmail.com>
2025-08-06 20:31:51 +0200

    fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+    elif length == 1:
+        print("[0]")
+        return
+    elif length == 2:
+        print("[0, 1]")
+        return
+    else:
+        print("[", end="")
+        a, b = 0, 1
+        for i in range(length):
+            if i > 0:
+                print(", ", end="")
+            print(a, end="")
+            a, b = b, a + b
+        print("]")
+        return
+        print("[]")
+print_fibonacci(9)
+    
